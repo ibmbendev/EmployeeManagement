bd599a12de6aba7f1c2810885511850ad5da28c3
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2015-01-16 23:18:43 +0530

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name employeeManagementV20App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the employeeManagementV20App
+ */
+angular.module('employeeManagementV20App')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
